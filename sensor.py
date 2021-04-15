ddbc2c593619c978990928c9f6e72a5485928ce5
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-04-15 12:42:55 +0200

    first real tesst

--- sensor.py
+++ sensor.py
@@ -1,1 +1,28 @@
-print("OCCUPANCY_NOT_DETECTED")
+import os
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
+
+wert = chan0.value
+
+if wert < 4000:
+    print("OCCUPANCY_DETECTED")
+    
+if wert > 4000:
+    print("OCCUPANCY_NOT_DETECTED")
+
+
