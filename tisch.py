f365f4525d232466e216c389ba031c9baf897fae
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-04-15 16:40:21 +0200

    anfahr abbrems compensation

--- tisch.py
+++ tisch.py
@@ -1,8 +1,37 @@
-#PROBLEM tisch färht in kleinen schritten weiter als in grossen liegt am anfahren 
-import sys
+ import sys
 import time
 characteristic = sys.argv[3].strip("''")
 
+accbrakecompensation = 0.5
+totaltime = 4 - accbrakecompensation #fahrzeit von 0-100 von unten bis ganz oben
+
+singlesteptime = totaltime/100
+diff = 0
+
+def go():
+    import RPi.GPIO as GPIO
+    GPIO.setwarnings(False)
+    runter = 15
+    hoch = 14
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(runter,GPIO.OUT)
+    GPIO.output(runter,0)
+    GPIO.setup(hoch,GPIO.OUT)
+    GPIO.output(hoch,0)
+
+    if diff < 0 and abs(diff) > 10:
+        ontime = abs(diff)*singlesteptime + accbrakecompensation
+        GPIO.output(runter,1)
+        time.sleep(ontime)
+        GPIO.cleanup()
+        
+    if diff > 0 and diff > 10:
+        ontime = diff*singlesteptime + accbrakecompensation
+        GPIO.output(hoch,1)
+        time.sleep(ontime)
+        GPIO.cleanup()
+
+    sys.exit() #if diff 0 oder kleiner als 10 nichts tun
 
 if sys.argv[1] == "Get":
     if characteristic == "On":
@@ -26,66 +55,29 @@ if sys.argv[1] == "Get":
 if sys.argv[1] == "Set":
     value = sys.argv[4].strip("''")
     value = str(value)
-    
-    import RPi.GPIO as GPIO
-    GPIO.setwarnings(False)
-    runter = 15
-    hoch = 14
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(runter,GPIO.OUT)
-    GPIO.output(runter,0)
-    GPIO.setup(hoch,GPIO.OUT)
-    GPIO.output(hoch,0)
 
-    totaltime = 4 #fahrzeit von 0-100 von unten bis ganz oben
+    f = open("/home/pi/Desktop/git/height.txt", 'r')
+    status = f.read() #lies alten höhen wert
+    f.close()
 
     if characteristic == "On":
         if value == "0":
-            #fahr tisch auf 0 wenn
-            #print("runter")
-            #print(totaltime)
-            GPIO.output(runter,1)
-            time.sleep(totaltime)
-            GPIO.cleanup()
+            diff = 0 - int(status) #fahr tisch auf null   
+
             f = open("/home/pi/Desktop/git/height.txt", 'w')
             f.write(value) #schrieb höhe null in height.txt
             f.close
+            
+            go()
+
         sys.exit() #wenn value 1 also anschalten tu nichts
 
     if characteristic == "Brightness":
-          f = open("/home/pi/Desktop/git/height.txt", 'r')
-          status = f.read() #lies alten höhen wert
-          f.close()
-          onesteptime = totaltime/100
-          diff = int(value) - int(status)
-          
-          #fahr runter
-          if diff < 0:
-              diff = abs(diff)
-              ontime = diff*onesteptime
-              #print("runter")
-              #print(ontime)
-              GPIO.output(runter,1)
-              time.sleep(ontime)
-              GPIO.cleanup()
+        diff = int(value) - int(status)
 
-              f = open("/home/pi/Desktop/git/height.txt", 'w')
-              f.write(value) #schrib neuen höhen wert
-              f.close
-              sys.exit()
+        f = open("/home/pi/Desktop/git/height.txt", 'w')
+        f.write(value) #schrib neuen höhen wert
+        f.close
 
-          #fahr hocch
-          if diff > 0:
-              ontime = diff*onesteptime
-              #print("hoch")
-              #print(ontime)
-              GPIO.output(hoch,1)
-              time.sleep(ontime)
-              GPIO.cleanup()
+        go()
 
-              f = open("/home/pi/Desktop/git/height.txt", 'w')
-              f.write(value) #schrib neuen höhen wert
-              f.close
-              sys.exit()
-          
-          sys.exit() #wenn diff 0 tu nichts
