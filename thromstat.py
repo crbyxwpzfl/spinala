c500ece12075efd8e3df33c537fbfdfc524abce0
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-04-15 10:28:45 +0200

    more simple

--- thromstat.py
+++ thromstat.py
@@ -7,46 +7,29 @@ if sys.argv[1] == "Get":
         print("raspberry")
         sys.exit()
 
-    if characteristic == "CurrentHeatingCoolingState":
-        f = open("/home/pi/Desktop/CHCStatus.txt", 'r')
-        status = f.read()
-        f.close()
-        print(status)
-        sys.exit()
-
-    if characteristic == "TargetHeatingCoolingState":
-        f = open("/home/pi/Desktop/THCStatus.txt", 'r')
-        status = f.read()
-        f.close()
-        print(status)
-        sys.exit()
-
-    if characteristic == "CurrentTemperature":
-        from gpiozero import CPUTemperature
-        cpu = CPUTemperature()
-        print(round(cpu.temperature))
-        sys.exit()
-
-    if characteristic == "TargetTemperature":
-        from gpiozero import CPUTemperature
-        cpu = CPUTemperature()
-        print(round(cpu.temperature))
-        sys.exit()
-
     if characteristic == "TemperatureDisplayUnits":
         print("CELSIUS")
-        sys.exit()
-
-    if characteristic == "CoolingThresholdTemperature":
-        from gpiozero import CPUTemperature
-        cpu = CPUTemperature()
-        print(round(cpu.temperature))
         sys.exit()
 
     if characteristic == "HeatingThresholdTemperature":
         print("0")
         sys.exit() 
+    
+    if characteristic == "CurrentHeatingCoolingState" or characteristic == "TargetHeatingCoolingState":
+        f = open("/home/pi/Desktop/git/Status.txt", 'r')
+        status = f.read()
+        f.close()
+        print(status)
+        sys.exit()
 
+    if characteristic == "CurrentTemperature" or characteristic == "TargetTemperature" or characteristic == "CoolingThresholdTemperature":
+        from gpiozero import CPUTemperature
+        cpu = CPUTemperature()
+        print(round(cpu.temperature))
+        
+        #logic für auto start fan
+
+        sys.exit() 
 
 if sys.argv[1] == "Set":
 
@@ -61,34 +44,13 @@ if sys.argv[1] == "Set":
         GPIO.setup(fan,GPIO.OUT)
 
         #off
-        if value == "0":
+        if value == "0" or value == "3":
                 #not handled on purpose
                 sys.exit()
         #heat
-        if value == "1":
+        if value == "1" or value == "2":
                 GPIO.output(fan,0)
-                f = open("/home/pi/Desktop/THCStatus.txt", 'w')
+                f = open("/home/pi/Desktop/git/Status.txt", 'w')
                 f.write(value)
                 f.close
-                
-                f = open("/home/pi/Desktop/CHCStatus.txt", 'w')
-                f.write(value)
-                f.close
-                sys.exit()
-
-        #cool
-        if value == "2":
-                GPIO.output(fan,1)
-                f = open("/home/pi/Desktop/THCStatus.txt", 'w')
-                f.write(value)
-                f.close
-                
-                f = open("/home/pi/Desktop/CHCStatus.txt", 'w')
-                f.write(value)
-                f.close
-                sys.exit()
-
-        #auto
-        if value == "3":
-                #not handled on purpose
                 sys.exit()
