1de636e3d93f3052719a2244784e437b7726fdd9
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-04-20 13:40:40 +0200

    test1

--- wall.py
+++ wall.py
@@ -20,7 +20,7 @@ def status():
     try:
         response = requests.get(f'https://{privates.ip}:1926/6/powerstate', verify=False, timeout=2, auth=HTTPDigestAuth(privates.user, privates.pw))
     except ConnectionError:
-        print(status)
+        print("hallohallo")
         output = subprocess.Popen(["sudo /etc/raspap/hostapd/servicestart.sh --seconds 3"], shell = True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         sys.exit()
     else:
