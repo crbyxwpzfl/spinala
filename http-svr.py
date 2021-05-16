586fdd95b8bfb6986c2082a7c84e655f23686e86
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-05-16 17:28:14 +0200

    typo

--- http-svr.py
+++ http-svr.py
@@ -4,23 +4,23 @@ import time
 
 
 class Server(BaseHTTPRequestHandler):
-def do_GET(self):
-    if self.path == '/':
-        self.path = '/index.html'
-    try:
-        split_path = os.path.splitext(self.path)
-        request_extension = split_path[1]
-        if request_extension != ".py":
-            f = open(self.path[1:]).read()
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(bytes(f, 'utf-8'))
-        else:
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            split_path = os.path.splitext(self.path)
+            request_extension = split_path[1]
+            if request_extension != ".py":
+                f = open(self.path[1:]).read()
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(bytes(f, 'utf-8'))
+            else:
+                f = "File not found"
+                self.send_error(404,f)
+        except:
             f = "File not found"
             self.send_error(404,f)
-    except:
-        f = "File not found"
-        self.send_error(404,f)
 
 HOST_NAME = '192.168.178.193'
 PORT = 8000
