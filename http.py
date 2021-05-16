d0b3338c0710af0f01285d6e70298e857841af6d
crbyxwpzfl <60987359+crbyxwpzfl@users.noreply.github.com>
2021-05-16 11:39:26 +0200

    try http server with python

---
+++ http.py
@@ -0,0 +1,65 @@
+import RPi.GPIO as GPIO
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+host_name = '192.168.172.193'    # Change this to your Raspberry Pi IP address
+host_port = 8000
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
+
+    def do_HEAD(self):
+        """ do_HEAD() can be tested use curl command 
+            'curl -I http://server-ip-address:port' 
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        """ do_GET() can be tested using curl command 
+            'curl http://server-ip-address:port' 
+        """
+        html = '''
+            <html>
+            <body style="width:960px; margin: 20px auto;">
+            <h1>Welcome to my Raspberry Pi</h1>
+            <p>Current GPU temperature is {}</p>
+            <form action="/" method="POST">
+                Turn LED :
+                <input type="submit" name="submit" value="On">
+                <input type="submit" name="submit" value="Off">
+            </form>
+            </body>
+            </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        """ do_POST() can be tested using curl command 
+            'curl -d "submit=On" http://server-ip-address:port' 
+        """
+        content_length = int(self.headers['Content-Length'])    # Get the size of data
+        post_data = self.rfile.read(content_length).decode("utf-8")   # Get the data
+        post_data = post_data.split("=")[1]    # Only keep the value
+        
+        # GPIO setup
+
+        if post_data == 'On':
+            print("post_Data ist on")
+        else:
+            print("post_data ist nicht On")    
+        print("LED is {}".format(post_data))
+        self._redirect('/')    # Redirect back to the root url
