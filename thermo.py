f7614de70a9f3f6fa4f9101c4f65f67a03f7ad43
f <socials.fl@icloud.com>
2022-05-01 19:42:51 +0200

    fucking gpiozero auto cleanup took ages to find

--- thermo.py
+++ thermo.py
@@ -1,103 +1,14 @@
-#import privates variable
 import sys
+import RPi.GPIO as GPIO
+import os
 
-characteristic = sys.argv[3].strip("''")
+def Get():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(d['fanpin'],GPIO.OUT)
+    GPIO.output(d['fanpin'], True) if d['CurrentTemperature'] > 30 else GPIO.output(d['fanpin'], False) # default case run fan 
+    print(d.get(sys.argv[3].strip("''") , GPIO.input(d['fanpin']) + 1 )) # prints 'CurrentHeatingCoolingState' 'TargetHeatingCoolingState' states 1 heat or 2 cool or from dict
 
-Statuspath = "/home/pi/spinala/Status.txt"
-
-if sys.argv[1] == "Get":
-
-    if characteristic == "Name":
-        print("raspberry")
-        sys.exit()
-
-    if characteristic == "TemperatureDisplayUnits":
-        print("CELSIUS")
-        sys.exit()
-
-    if characteristic == "HeatingThresholdTemperature":
-        print("0")
-        sys.exit()
-
-    if  characteristic == "TargetTemperature" or characteristic == "CoolingThresholdTemperature":
-        print("10")
-        sys.exit()
-    
-    if characteristic == "CurrentHeatingCoolingState" or characteristic == "TargetHeatingCoolingState":
-        f = open(Statuspath, 'r')
-        status = f.read()
-        f.close()
-        
-        import RPi.GPIO as GPIO
-        GPIO.setwarnings(False)
-        fan = 21
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(fan,GPIO.OUT)
-        
-        #cool
-        if int(status) == 2:
-            GPIO.output(fan,1)
-            
-        #heat
-        if int(status) == 1:
-            GPIO.output(fan,0)
-            
-        print(status)
-        sys.exit()
-
-    if characteristic == "CurrentTemperature":
-        
-        #read cpu temp
-        from gpiozero import CPUTemperature
-        cpu = CPUTemperature()
-        cputemp = round(cpu.temperature)
-        
-        #set status to cooling
-        if cputemp > 40:
-            f = open(Statuspath, 'w')
-            f.write("2") #status cool
-            f.close
-
-    
-        #set status to heating
-        if cputemp < 30:
-            f = open(Statuspath, 'w')
-            f.write("1") #status heat
-            f.close
-
-
-        print(cputemp)
-        sys.exit() 
-
-if sys.argv[1] == "Set":
-
-    if characteristic == "TargetHeatingCoolingState":
-        value = sys.argv[4].strip("''")
-        value = int(value)
-            
-        import RPi.GPIO as GPIO
-        GPIO.setwarnings(False)
-        fan = 21
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(fan,GPIO.OUT)
-
-        #off
-        if int(value) == 0 or int(value) == 3:
-            #not handled on purpose
-            sys.exit()
-
-        #heat
-        if int(value) == 1:
-            GPIO.output(fan,0)
-            f = open(Statuspath, 'w')
-            f.write(value)
-            f.close
-            sys.exit()
-
-        #cool
-        if int(value) == 2:
-            GPIO.output(fan,1)
-            f = open(Statuspath, 'w')
-            f.write(value)
-            f.close
-            sys.exit()
+GPIO.setwarnings(False)
+d = {'Get': Get, 'fanpin': 21, 'CurrentTemperature': int(os.popen('vcgencmd measure_temp').readline()[5:-5]),'TemperatureDisplayUnits': 'CELSIUS', 'HeatingThresholdTemperature': 0, 'TargetTemperature': 10, 'CoolingThresholdTemperature': 10} # set 'pin'
+d.get(sys.argv[1].strip("''"), sys.exit)() # call 'Get' or 'Set'
+#GPIO.cleanup() # lots of warnings with out cleanup but clenup doesnt keep fan running
