98768e34228717c2192730f142fa4bb73a2378da
f <socials.fl@icloud.com>
2022-08-07 15:25:55 +0200

    good enugh

--- tisch.py
+++ tisch.py
@@ -8,16 +8,12 @@ def sub(cmdstring, waitforcompletion): # string here because shell true because 
      if waitforcompletion: return p.communicate()[0].decode() # this will wait for subprocess to finisih
 
 def move(): # in the right direction
-    while sense() < int(sys.argv[4]) - 3: # lift when position delta pos and 'downpin' off including some correction values
-        GPIO.output(d['downpin'], False) # here incase of changed direction
-        GPIO.output(d['uppin'], True)
-
-    while sense() + 10 > int(sys.argv[4]) + 3: # lowwer when position delta neg and 'uppin' off
-        GPIO.output(d['uppin'], False) # here incase of changed direction
-        GPIO.output(d['downpin'], True)
+    while sense() not in range( int(sys.argv[4]) - 10 , int(sys.argv[4]) ): # some fine tuning is to be done to reach true zero every time here
+        if d['h'] > int(sys.argv[4]): GPIO.output(d['uppin'], False); GPIO.output(d['downpin'], True)
+        if d['h'] < int(sys.argv[4]): GPIO.output(d['uppin'], True); GPIO.output(d['downpin'], False)
 
     GPIO.output(d['downpin'], False) # stop driving
-    GPIO.output(d['uppin'], False) # stop driving
+    GPIO.output(d['uppin'], False)
 
 def sense(): # current pos via sensor
     GPIO.output(d['triggerpin'], True)
@@ -35,15 +31,11 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # gpio Modus BOARD or BCM
 GPIO.setup(d['echopin'], GPIO.IN) # 'echopin' input
 GPIO.setup(d['triggerpin'], GPIO.OUT) # 'triggerpin' output
-GPIO.setup(d['uppin'], GPIO.OUT) # init move pins
-GPIO.setup(d['downpin'], GPIO.OUT)
 
+if sys.argv[1] == 'Set': GPIO.setup(d['uppin'], GPIO.OUT) # init move pins just on set so that set can run in while get senses pos
+if sys.argv[1] == 'Set': GPIO.setup(d['downpin'], GPIO.OUT)
 
-# set move pins to off somwhere here otherwise they keep state and reactivae on new script run
-
-
-
-if sub('pgrep -lfc tisch.py', True).strip('\n') > 1: sub('pkill -of tisch.py', True) # kill oldest tisch.py when more than 2 are running
+if sub('pgrep -lfc tisch.py', True).strip('\n') > 1 and sys.argv[1] == 'Set': sub('pkill -of tisch.py', True) # kill oldest tisch.py when more than 2 are running
 
 if not sys.argv[4:] or sys.argv[4:] != ['1']: d.get(sys.argv[1].strip("''"))() # call 'Get' or 'Set'
 
