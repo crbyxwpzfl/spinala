3db5d2878d1c24da6241a9de322e8cd018e76500
f <socials.fl@icloud.com>
2023-03-09 15:32:04 +0100

    little helper to read sensor

--- tisch.py
+++ tisch.py
@@ -19,13 +19,13 @@ async def serve():  # initiates listening for client connections and calls move
 async def setupvl53():
     import os;    os.environ["BLINKA_MCP2221"] = "1";    import board, digitalio, adafruit_vl53l1x;    d['vl53'] = adafruit_vl53l1x.VL53L1X(board.I2C());    d['vl53'].start_ranging()  # configure sense some how eventough this takes 1sec no client connections are lost in the mean time
 
-async def move():  # moves tisch
+async def move():  # this server respondes to 'Set/Get whatever whatever int' and drives tisch to int. int can be 'R+\{1}'
     while not math.isclose( d.get('position', 1111) , d['dsrd'] , abs_tol=5):  # TODO calculate tolerance including round()
         d['rawpos'] = d['range']();    d['transpos'] = d['translate'](d['rawpos'])
-        print(d['lineup'], end=d['lineclear']);    print(d['lineup'], end=d['lineclear']);    print("this server respondes to 'Set/Get whatever whatever int' and drives tisch to int. int can be 'R+\{1}'");    print(f"rawpos-{d['rawpos']} transpos-{d['transpos']} desired-{d['dsrd']} ")  # pretty print
-        if d['transpos'] < d['dsrd'] and not d.get('moving') == "up": d['moving'] = "up";    d['drivepin'](eval(d['pindw']), False);    d['drivepin'](eval(d['pinup']), True)
-        if d['transpos'] > d['dsrd'] and not d.get('moving') == "dw": d['moving'] = "dw";    d['drivepin'](eval(d['pinup']), False);    d['drivepin'](eval(d['pindw']), True)
-        await asyncio.sleep(0.5)
+        print(f"rawpos-{d['rawpos']} transpos-{d['transpos']} desired-{d['dsrd']} ")  # pretty print 'lineup': '\033[1A', 'lineclear': '\x1b[2K'  print(d['lineup'], end=d['lineclear']);
+        if d['transpos'] < d['dsrd'] < 101 and not d.get('alrdymovng') == "up": d['alrdymovng'] = "up";    d['drivepin'](eval(d['pindw']), False);    d['drivepin'](eval(d['pinup']), True)
+        if d['transpos'] > d['dsrd'] < 101 and not d.get('alrdymovng') == "dw": d['alrdymovng'] = "dw";    d['drivepin'](eval(d['pinup']), False);    d['drivepin'](eval(d['pindw']), True)
+        await asyncio.sleep(0.5)  # to just read sensor spawn serve instance via 'python3 ....py serve 101' so pins dont get polled high or low
 
 async def trysend():  # 1 will be ignored since for every 'Set tisch Brightness int' homebridge also does 'Set tisch On 1' wich would overwrite brightness int
     try: socket.create_connection(('localhost', 2222)).sendall(bytes(sys.argv[4], "utf-8")) if sys.argv[4] != '1' else sys.exit()  # try to connect to serve() socket instance and send desired pos 
@@ -35,11 +35,11 @@ async def tryrecv():  # try to asks serve instance for 'int' except ranges int
     try: d['transpos'] = socket.create_connection(('localhost', 2222)).recv(255).decode();    print( max( min(d['transpos'],1), 0) ) if sys.argv[3] == 'On' else print(d['transpos'])  # TODO peraps change this to raw pos and round or do max(d['rawpos'],0) to report correct hight and off state
     except: await setupvl53();    d['rawpos'] = d['range']();    d['transpos'] = d['translate'](d['rawpos']);    print( max( min(d['transpos'],1), 0) ) if sys.argv[3] == 'On' else print(d['transpos'])
 
-d = {'Set': trysend, 'Get': tryrecv,'serve': serve, 'pinup': "board.G1", 'pindw': "board.G0",
+d = {'cali': debug, 'Set': trysend, 'Get': tryrecv,'serve': serve, 'pinup': "board.G1", 'pindw': "board.G0",
      'drivepin': lambda pin, val: digitalio.DigitalInOut(pin).switch_to_output(value=val),
      'translate': lambda h: (h - 79 )*100/(140-79),  # TODO put round() here somewhere
      'range': lambda: d['vl53'].distance if d['vl53'].data_ready and d['vl53'].distance else 1111,  # vl53.clear_interrupt() is missing but thoretically interrupt is not relevant
-     'lineup': '\033[1A', 'lineclear': '\x1b[2K'}
+    }
 
 asyncio.run( d.get(sys.argv[1])() )  # call move
 
